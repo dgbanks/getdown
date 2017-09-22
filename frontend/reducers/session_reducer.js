eee1fc2495f5e832c82e39e17f3391b045c8c355
David <davidgbanks91@gmail.com>
2017-09-22 09:13:20 -0700

    Prepare for initial Heroku push

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -2,7 +2,6 @@ import {RECEIVE_CURRENT_USER} from '../actions/session_actions';
 import merge from 'lodash/merge';
 
 export const SessionReducer = (prevState = {currentUser: null}, action) => {
-  console.log(action.currentUser);
   Object.freeze(prevState);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
