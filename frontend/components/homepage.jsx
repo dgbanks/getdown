d9f44b188bac2d75c7dd070115241d3da4bf3328
David <davidgbanks91@gmail.com>
2017-11-07 22:43:06 -0800

    finished splash animation

--- frontend/components/homepage.jsx
+++ frontend/components/homepage.jsx
@@ -3,38 +3,39 @@ import GroupSearchContainer from './group_search/group_search_container';
 import EventIndexContainer from './event_index/event_index_container';
 
 const randomColor = () => {
-  return ['red', 'blue', 'green', 'yellow', 'purple', 'orange'][Math.floor(Math.random() * 4)];
+  return ['red', 'blue', 'green', 'yellow', 'purple', 'orange'][Math.floor(Math.random() * 6)];
 };
 
 const randomOpacity = () => {
-  return ['.3', '.4', '.5', '.6', '.7'][Math.floor(Math.random() * 5)];
+  return ['.2', '.3', '.4', '.5', '.6', '.7', '.8'][Math.floor(Math.random() * 7)];
 };
 
 const randomWidth = () => {
   return ['500', '750', '1000', '1250', '1500'][Math.floor(Math.random() * 5)];
 };
 
-const randomTime = () => {
-  return ['5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'][Math.floor(Math.random() * 11)];
+const randomDelay = () => {
+  return ['0', '.1', '.2', '.3',
+                '.4', '.5', '.6', '.7',
+                '.8', '.9', '1'][Math.floor(Math.random() * 11)];
 };
 
-const pxOrPercent = () => {
-  const array = [['-', 'px'], ['', '%']];
-  const result = [];
-  let num = Math.floor(Math.random() * 2);
-  result.push(array[num]);
-  result.push(array[num - 1]);
-  return result;
+const randomTime = () => {
+  return ['5', '5.25', '5.5', '5.75',
+          '6', '6.25', '6.5', '6.75',
+          '7', '7.25', '7.5', '7.75',
+          '8', '8.25', '8.5', '8.75',
+          '9', '9.25', '9.5', '9.75',
+          '10'][Math.floor(Math.random() * 21)];
 };
 
 const insertKeyframes = (width) => {
-  const syms = pxOrPercent();
-  const styleSheet = document.styleSheets[0];
-  const keyframes = `@keyframes moveStripe {
+  let styleSheet = document.styleSheets[0];
+  let keyframes = `@keyframes moveStripe {
     from
-    { margin-left: ${syms[0][0]}${width}${syms[0][1]}; }
+    { margin-left: -${width}px; }
     to
-    { margin-left: ${syms[1][0]}${width}${syms[1][1]}; }
+    { margin-left: 100%; }
     }`;
 
   styleSheet.insertRule(keyframes);
@@ -44,25 +45,30 @@ class Homepage extends React.Component {
   constructor() {
     super();
     this.color = randomColor();
+    this.width = randomWidth();
     this.margin = 0;
   }
 
   getRandomStyles() {
-    insertKeyframes();
+    // let width = randomWidth();
+    insertKeyframes(this.width);
+
     return (
       {
         position: 'absolute',
         height: '50px',
         zIndex: '-1',
-        backgroundColor: this.color,
+        backgroundColor: randomColor(),
         opacity: randomOpacity(),
-        width: randomWidth(),
+        width: `${this.width}px`,
         marginTop: `${this.margin}px`,
-        animationDuration: `${randomTime()}s`,
         animationName: 'moveStripe',
+        animationDuration: `${randomTime()}s`,
+        // animationDelay: `${randomDelay()}s`,
+        // animationFillMode: 'none',
         animationIterationCount: 'infinite',
-        animationDirection: 'alternate',
-        // animation: `${randomTime()}s linear 0s infinite { from { margin-left: -20%; } to { margin-left: 100%; }  }`
+        animationDirection: 'alternate-reverse',
+        animationTimingFunction: 'linear'
       }
     );
   }
@@ -86,25 +92,23 @@ class Homepage extends React.Component {
   }
 
   render() {
-    let array = this.tenStripes();
+    // let array = this.tenStripes();
     return (
       <div className='homepage'>
 
         <div className='splash-container'>
 
-          {array}
+          {this.tenStripes()}
 
           <div className='splash-text'>
             <h1>Looking for something to do?</h1>
-            <h1>find a group you can <h2>getdown</h2> with</h1>
+            <h1>find a group you can <h2 style={{color: this.color, fontWeight: 'bold'}}>getdown</h2> with</h1>
           </div>
 
-
-        </div>
-
-        <div>
           <GroupSearchContainer />
+
         </div>
+
         <div>
           <EventIndexContainer />
         </div>
